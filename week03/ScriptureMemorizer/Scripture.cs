0cb1f0d73f50e1b7ef2ef5f2aee3750936e880b4
maguilar08 <Marcelo_AguilarSBS@outlook.com>
2025-07-30 17:58:36 -0400

    Team activity done

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+    public bool IsCompletelyHidden()
+    {
+        return true; //just to compile        
+    }
+}
