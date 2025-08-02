a8e2b4c773e5333de9dab9d66a9def0a87cc8375
maguilar08 <Marcelo_AguilarSBS@outlook.com>
2025-08-02 19:57:49 -0400

    Memorizer just to finish

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        //Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him,and he shall direct thy paths.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            if (scripture.IsCompletelyHidden())
+                break;
+
+            scripture.HideRandomWords(3); 
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+
+
     }
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 public class Word
@@ -7,24 +8,38 @@ public class Word
 
     public Word(string text)
     {
+        _isHidden = false;
+        _text = text;
 
     }
+
     public void Hide()
     {
-
+        _isHidden = true;
     }
+
     public void Show()
     {
-
+        _isHidden = false;
     }
+
     public bool IsHidden()
     {
-        return true; //just to compile
+        //return true; just to compile
+        return _isHidden;
     }
+
     public string GetDisplayText()
     {
-        return "";
+        if (_isHidden)
+        {
+            int numberOfLetters = _text.Length;
+            string underscores = new string('_', numberOfLetters); //Don´t use ("") instead of that use this ('')
+            return underscores;
+        }
+        else
+        {
+            return _text;
+        }
     }
-
-
 }
